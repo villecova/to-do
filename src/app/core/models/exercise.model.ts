10fc40069dbe02503f4a92707c6b681582150224
villecova <villecova@gmail.com>
2025-04-28 12:11:30 -0600

    model update

--- src/app/core/models/exercise.model.ts
+++ src/app/core/models/exercise.model.ts
@@ -1,8 +1,34 @@
-export interface Exercise {
+export interface UserWeight {
+  userName: string
+  weight: number
+}
+
+export interface WorkoutList {
   id: string
   name: string
   reps: number
   sets: number
-  weight: number
+  weight: number | UserWeight[] // Single weight for personal, array for cooperative
   completed?: boolean
+}
+
+export interface Workout {
+  id: string
+  name: string
+  description?: number
+  type: 'personal' | 'cooperative'
+  exercises: WorkoutList[]
+}
+
+export interface Exercise {
+  id: string
+  name: string
+  description: string
+  type: 'personal' | 'shared'
+  createdBy: string
+  createdAt: Date
+  sets: number
+  reps: number
+  weight?: number
+  notes?: string
 }
